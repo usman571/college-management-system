b8cfde2b7ce770ebd9b3bbbae1f687748325af64
usman571 <developer2119181@gmail.com>
2025-01-20 02:06:44 +0500

    middlerware change

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,32 +15,20 @@
 // });
 
 // export const config = { matcher: ['/dashboard/:path*'] };
+// Protecting routes with next-auth
+// https://next-auth.js.org/configuration/nextjs#middleware
+// https://nextjs.org/docs/app/building-your-application/routing/middleware
 
 import NextAuth from 'next-auth';
 import authConfig from '@/lib/auth.config';
-import { NextResponse } from 'next/server';
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  const isLoggedIn = !!req.auth;
-  const { pathname, origin, href } = req.nextUrl;
-
-  if (!isLoggedIn) {
-    // For protected routes, redirect to login
-    if (pathname.startsWith('/dashboard')) {
-      return NextResponse.redirect(new URL('/login', href));
-    }
-  } else {
-    // If logged in and trying to access login page, redirect to dashboard
-    if (pathname === '/login') {
-      return NextResponse.redirect(new URL('/dashboard', href));
-    }
+  if (!req.auth) {
+    const url = req.url.replace(req.nextUrl.pathname, '/');
+    return Response.redirect(url);
   }
-
-  return NextResponse.next();
 });
 
-export const config = {
-  matcher: ['/dashboard/:path*', '/login']
-};
+export const config = { matcher: ['/dashboard/:path*'] };
