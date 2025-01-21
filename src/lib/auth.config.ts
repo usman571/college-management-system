9117abc928c0da75f875de89db9c0e4421001581
usman571 <developer2119181@gmail.com>
2025-01-21 22:16:50 +0500

    change auth file

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,45 +1,82 @@
+// import { NextAuthConfig } from 'next-auth';
+// import CredentialProvider from 'next-auth/providers/credentials';
+// import GithubProvider from 'next-auth/providers/github';
+
+// const authConfig = {
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID ?? '',
+//       clientSecret: process.env.GITHUB_SECRET ?? ''
+//     }),
+//     CredentialProvider({
+//       credentials: {
+//         email: {
+//           type: 'email'
+//         },
+//         password: {
+//           type: 'password'
+//         }
+//       },
+//       async authorize(credentials, req) {
+//         if (process.env.NEXT_PUBLIC_FAKE_LOGIN === 'true') {
+//           return { id: '1', name: 'Admin', email: 'dummy@admin.com' };
+//         }
+//         const user = {
+//           id: '1',
+//           name: 'John',
+//           email: credentials?.email as string
+//         };
+//         if (user) {
+//           // Any object returned will be saved in `user` property of the JWT
+//           return user;
+//         } else {
+//           // If you return null then an error will be displayed advising the user to check their details.
+//           return null;
+
+//           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+//         }
+//       }
+//     })
+//   ],
+//   pages: {
+//     signIn: '/' //sigin page
+//   }
+// } satisfies NextAuthConfig;
+
+// export default authConfig;
+
 import { NextAuthConfig } from 'next-auth';
 import CredentialProvider from 'next-auth/providers/credentials';
-import GithubProvider from 'next-auth/providers/github';
+import { User } from 'next-auth';
 
 const authConfig = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? '',
-      clientSecret: process.env.GITHUB_SECRET ?? ''
-    }),
     CredentialProvider({
       credentials: {
-        email: {
-          type: 'email'
-        },
-        password: {
-          type: 'password'
-        }
+        email: { type: 'email' }
       },
-      async authorize(credentials, req) {
-        if (process.env.NEXT_PUBLIC_FAKE_LOGIN === 'true') {
-          return { id: '1', name: 'Admin', email: 'dummy@admin.com' };
-        }
-        const user = {
+      async authorize(credentials): Promise<User | null> {
+        // Always return a dummy user for testing
+        const user: User = {
           id: '1',
-          name: 'John',
+          name: 'Test User',
           email: credentials?.email as string
         };
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
+        return user;
       }
     })
   ],
   pages: {
-    signIn: '/' //sigin page
+    signIn: '/'
+  },
+  callbacks: {
+    redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    }
   }
 } satisfies NextAuthConfig;
 
